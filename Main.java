c9be8b00f7eaa36ca24a1bbe1ec5fde554021194
EkaterinaAli <105931947+EkaterinaAli@users.noreply.github.com>
2022-06-28 20:52:16 +0300

    Урок 1
    Сделала урок 1

---
+++ Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Begin 1 Дана сторона квадрата а. Найти его периметр Р=4*а
+     /*   int a = 5;
+        // вычисление результат
+        int P = 4 * a;
+        //вывод результат
+        System.out.println(P);
+    */
+       /* //Дана сторона квадрата а. Найдите его площадь S = a**2
+    int a=6;
+    int S =a*a;
+    System.out.println("У квадрата со стороной " + a + " площадь "+ S);*/
+/*
+       // begin 4 Дан диаметр окружности d. Найти ее длину L=п*d. В качестве
+        // значения использовать п использовать 3,14.
+        int d = 18;
+        final double pi = 3.14;
+        double L = pi * d;
+        System.out.println("У круга с диаметром " + d + " длина окружности " + L);
+        */
+      /*// begin 5 дана длина ребра куба а. Найти объем куба V=a^3  и площадь его
+      // поверхности S = 6*a2
+      int a = 5;
+      int v = a*a*a;
+      int S = a*a*6;
+
+        System.out.println ("Объем куба:" + v);
+        System.out.println( "Площадь поверхности куба: " +S );
+
+       */
+     /* // begin 6 Дана длина ребра a, b, c прямоугольного параллелепипеда. Найти его
+      //его объем V =a*b*c и площадь поверхности S = 2·(a·b + b·c + a·c).
+        int a=2;
+        int b=3;
+        int c=4;
+        int V=a*b*c;
+        int S=2*(a*b+b*c+a*c);
+        System.out.println(" V " + V);
+        System.out.println(" S " + S);*/
+
+     /*//Begin8 Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2.
+
+        int a = 3;
+        int b = 4;
+        int d = 2;
+        double c = (a+b)/(double)d;
+        System.out.println("Срднее арифметическое: " + c);*/
+
+      //
+
+
+
+    }
+}
