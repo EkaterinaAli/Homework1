ae62441033006485e11a599e7452b67c07a335ba
EkaterinaAli <105931947+EkaterinaAli@users.noreply.github.com>
2022-07-07 21:34:29 +0300

    УРОК 4

--- Main.java
+++ Main.java
@@ -1,124 +1,226 @@
-package com.company;
-
-import java.sql.SQLOutput;
-
+package Lesson4;
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        // Begin 1 Дана сторона квадрата а. Найти его периметр Р=4*а
-     /*   int a = 5;
-        // вычисление результат
-        int P = 4 * a;
-        //вывод результат
-        System.out.println(P);
-    */
-       /* //Дана сторона квадрата а. Найдите его площадь S = a**2
-    int a=6;
-    int S =a*a;
-    System.out.println("У квадрата со стороной " + a + " площадь "+ S);*/
-/*
-       // begin 4 Дан диаметр окружности d. Найти ее длину L=п*d. В качестве
-        // значения использовать п использовать 3,14.
-        int d = 18;
-        final double pi = 3.14;
-        double L = pi * d;
-        System.out.println("У круга с диаметром " + d + " длина окружности " + L);
-        */
-      /*// begin 5 дана длина ребра куба а. Найти объем куба V=a^3  и площадь его
-      // поверхности S = 6*a2
-      int a = 5;
-      int v = a*a*a;
-      int S = a*a*6;
+      /*  //   If1. Дано целое число. Если оно является положительным, то прибавить к нему 1;
+        //   в противном случае не изменять его. Вывести полученное число.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a > 0){
+            a = a + 1; //a++; ++a; // a--; --a;
+        }
+        System.out.println(a);
+                      */
+      //If2. Дано целое число. Если оно является положительным, то прибавить к нему 1;
+        // в противном случае вычесть из него 2. Вывести полученное число.
+       /* Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a > 0){
+            a = a + 1;}
+         else{a = a - 2;  // a -= 2;
+        }
+        System.out.println(a);*/
 
-        System.out.println ("Объем куба:" + v);
-        System.out.println( "Площадь поверхности куба: " +S );
+        // If3. Дано целое число. Если оно является положительным, то прибавить к нему 1;
+        // если отрицательным, то вычесть из него 2; если нулевым, то заменить его на 10. Вывести полученное число
+        /*Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a > 0) {
+            a=a + 1;
+        }
+        else {
+            if (a < 0) {
+                a -= 2;
+            } else {
+                a = 10;
+            }
+        }
+        System.out.println(a);
+
+         */
+
+      /*  //If4◦ Даны три целых числа. Найти количество положительных чисел в исходном наборе.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int res = 0;
+        if (a >0) {
+            res++;
+        }
+        if (b>0){
+            res++;
+        }
+        if (c>0)
+            {res++;
+            }
+        System.out.println(res);
 
        */
-     /* //  begin 6 Дана длина ребра a, b, c прямоугольного параллелепипеда. Найти его
-      //его объем V =a*b*c и площадь поверхности S = 2·(a·b + b·c + a·c).
-        int a=2;
-        int b=3;
-        int c=4;
-        int V=a*b*c;
-        int S=2*(a*b+b*c+a*c);
-        System.out.println(" V " + V);
-        System.out.println(" S " + S);*/
+     /* //If5. Даны три целых числа. Найти количество положительных и количество отрицательных чисел в исходном наборе.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int res = 0;
 
-     /*//Begin8 Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2.
+        if (a >0) {
+            res++;
+        }
+        if (b>0){
+            res++;
+        }
+        if (c>0)
+        {res++;
+        }
+        int otr =0;
 
-        int a = 3;
-        int b = 4;
-        int d = 2;
-        double c = (a+b)/(double)d;
-        System.out.println("Срднее арифметическое: " + c);*/
+        if (a <0) {
+            otr++;
+        }
+        if (b<0){
+            otr++;
+        }
+        if (c<0)
+        {otr++;
+        }
+        System.out.println(res);
+        System.out.println(otr);
 
-      /*//Даны два неотрицательных числа a и b. Найти их среднее геометрическое, то есть квадратный корень из их произведения: √
-        //a·b.
-        double a =3.45;
-        double b = 7.2;
-        double g_avg = Math.sqrt(a*b);
-        System.out.println("квадратный корень из их произведения " + g_avg);*/
+      */
+     /*//If6◦ Даны два числа. Вывести большее из них.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int res ;
+        if (a > b){
+            res = a;}
+        else {
+            res = b;
+                }
+        System.out.println(res);
+             */
 
-      //  begin 10  Даны два ненулевых числа. Найти сумму, разность, произведение и
-        //частное их квадратов.
-           /*
-        int a=25;
-        int b=45;
-        int s=(a*a)+(b*b);
-        int d=(a*a)-(b*b);
-        int r=(a*a)*(b*b);
-        double f=(a*a)/(double)(b*b);
-        System.out.println("Сумма: "+s);
-        System.out.println("Разность: "+d);
-        System.out.println("Произведение: "+r);
-        System.out.println("Частное: "+f);*/
+     /*// If7. Даны два числа. Вывести порядковый номер меньшего из них.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int res ;
+        if (a > b){
+            res = 2;}
+        else {
+            res = 1;
+        }
+        System.out.println(res);
 
-        /*  //Begin11◦
-        //. Даны два ненулевых числа. Найти сумму, разность, произведение и
-        //частное их модулей.
-        int a=-5;
-        int b=24;
-        a = Math.abs(a);
-        b = Math.abs(b);
-        double s = a+b;
-        System.out.println("Сумма модулей "+ (int) s);
-        s= a-b;
-        System.out.println(" Разность модулей "+ (int) s);
-        s = a*b;
-        System.out.println("Произведение модулей "+ (int) s);
-        s = (double)a/b;
-        System.out.println("Частное модулей "+ s);*/
+      */
+     /*// If6◦ Даны два числа. Вывести большее из них.
+           Scanner in = new Scanner(System.in);
+               int a = in.nextInt();
+               int b = in.nextInt();
+               int res ;
+               if (a>b){
+                   System.out.println(a);}
+                else {  if (a < b){
+                   System.out.println(b);}
+                   else
+                   System.out.println("=");
+                        }
 
-     /* //  Begin22◦ Поменять местами содержимое переменных A и B и вывести новые
-     //   значения A и B.
-      int a = 3;
-      int b = 10;
-      int c = a;
-       a = b;
-       b = c;
-        System.out.println("a =  " + a + " b = "+ b);*/
+      */
+     /*//If7◦ Даны два числа. Вывести порядковый номер меньшего из них.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int res ;
+        if (a < b){
+            System.out.println("1");}
+        else {  if (a > b){
+            System.out.println("2");}
+        else
+            System.out.println("=");
+        }*/
+        /*//If9. Даны две переменные вещественного типа: A, B. Перераспределить значения данных переменных так,
+        // чтобы в A оказалось меньшее из значений, а в B — большее. Вывести новые значения переменных A и B.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
 
-     /*// Даны катеты прямоугольного треугольника a и b. Найти его гипотенузу c и периметр P:
-        //c =√a2 + b2
-        //, P = a + b + c.
+        if (b < a ){ int c = a;
+                     a = b ;
+                     b = c;
+                    }
+        System.out.println(a + "  "+  b);*/
 
-        int a = 4;
-        int b = 8;
-        double c = Math.sqrt(a*a+b*b);
-        double p = a+b+c;
-        System.out.println("Гипотеза =  " + c + " периметр  "+ p); */
+        /*//If11. Даны две переменные целого типа: A и B. Если их значения не равны, то
+        //присвоить каждой переменной большее из этих значений, а если равны,
+        //то присвоить переменным нулевые значения. Вывести новые значения переменных A и B.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if (a != b){
+            if(a > b){
+                b = a;
+            }
+            else {
+                a = b;
+        }}
+        else {a = 0;
+              b = 0;
+        }
+            System.out.println(a + "  "+  b);
 
-     //Даны два круга с общим центром и радиусами R1 и R2 (R1 > R2).
-        //Найти площади этих кругов S1 и S2, а также площадь S3 кольца, внешний радиус которого равен R1, а внутренний
-        // радиус равен R2: S1 = π·(R1)2, S2 = π·(R2)2, S3 = S1 − S2.
+         */
+        //If12◦ Даны три числа. Найти наименьшее из них.
+        /*Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int res = 0;
 
-        int R1 = 20;
-        int R2 = 10;
-        final double pi = 3.14;
-        double S1 = pi * R1*R1;
-        double S2 = pi * R2*R2;
-        double S3 = S1-S2;
-        System.out.println("площадь s1 =  " + s1 + " периметр  "+ p); */
+        if (a < b && a < c) res = a;
+           else
+               if(b < a && b < c) res = b ;
+               else
+                    if(c < a && c < b) res =c;
+        System.out.println(res);
+
+         */
+        //If15. Даны три числа. Найти сумму двух наибольших из них.
+       /* Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int res = 0;
+
+        if (a < b && a < c) res = b + c;
+           else
+              if(b < a && b < c) res = a + c ;
+              else
+                   if(c < a && c < b) res = a + b;
+        System.out.println(res);
+
+        */
+
+       /*//If18. Даны три целых числа, одно из которых отлично от двух других, равных между собой.
+        // Определить порядковый номер числа, отличного от остальных.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int res = 0;
+        if (a==b) res = 3;
+        else if (a==c) res = 2;
+        else if (b==c) res =1;
+        System.out.println(res);
+
+        */
+
+
+
+
+
+
 
 
 
@@ -131,4 +233,7 @@ public class Main {
 
 
     }
+
+
+
 }
